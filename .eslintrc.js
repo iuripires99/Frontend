90109f322ce86ae712304cfa55154519968540dd
iuripires99 <133361855+iuripires99@users.noreply.github.com>
2024-07-12 13:08:25 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
   rules: {
     "no-console": "off",
-    extends: [
-      "@babel/plugin-transform-private-property-in-object",
-      "next/core-web-vitals",
-    ],
   },
 };
